0dd4226cd495a68b66ef15dc3f7315ae2fd85570
hassan <hassansobai6@gmail.com>
2026-01-25 15:17:47 +0100

    login

---
+++ cafi/src/main/java/module-info.java
@@ -0,0 +1,19 @@
+module com.example.cafi {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires net.synedra.validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+    requires com.almasb.fxgl.all;
+
+    // هاد السطر هو اللي كان ناقص باش Scene Builder يخدم مزيان [cite: 2025-12-30]
+    opens control to javafx.fxml;
+
+    opens com.example.cafi to javafx.fxml;
+    exports com.example.cafi;
+}
